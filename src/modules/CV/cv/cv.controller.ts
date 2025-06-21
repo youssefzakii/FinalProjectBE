7c54dc1a6f68c37075f2b7366fa72d2937035047
ahmed ezzat <ahmedalahly800@gmail.com>
2025-06-21 18:44:36 +0300

    sync uploading cv and analysis with database and adding getting cvs

--- src/modules/CV/cv/cv.controller.ts
+++ src/modules/CV/cv/cv.controller.ts
@@ -1,25 +1,40 @@
 import {
-    Controller,
-    Post,
-    Get,
-    UploadedFile,
-    UseInterceptors,
-  } from '@nestjs/common';
-  import { FileInterceptor } from '@nestjs/platform-express';
-  import { HandleCvService } from 'src/modules/services/handle-cv/handle-cv.service';
-  
-  @Controller('cv')
-  export class CvController {
-    constructor(private readonly handleCvService: HandleCvService) {}
-    @Get('test')
-    async test() {
-      return 'CV Module is working!';
-    }
-    
-    @Post('analyze')
-    @UseInterceptors(FileInterceptor('file'))
-    async analyze(@UploadedFile() file) {
-      return this.handleCvService.analyzeResume(file);
-    }
+  Controller,
+  Post,
+  Get,
+  UploadedFile,
+  UseInterceptors,
+  Req,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { HandleCvService } from 'src/modules/services/handle-cv/handle-cv.service';
+import { Request } from 'express';
+
+@Controller('cv')
+export class CvController {
+  constructor(private readonly handleCvService: HandleCvService) {}
+
+  @Get('test')
+  async test() {
+    return 'CV Module is working!';
   }
-  
+  @Get('getCvs')
+  async getCvs(@Req() req: Request) {
+    const username = req['user']?.username;
+    if (!username) {
+      throw new Error('User ID not found in token');
+    }
+
+    return this.handleCvService.getUserCvs(username);
+  }
+  @Post('analyze')
+  @UseInterceptors(FileInterceptor('file'))
+  async analyze(@UploadedFile() file, @Req() req: Request) {
+    const username = req['user']?.username;
+    if (!username) {
+      throw new Error('User ID not found in token');
+    }
+
+    return this.handleCvService.analyzeResume(file, username);
+  }
+}
--- src/modules/CV/cv/cv.module.ts
+++ src/modules/CV/cv/cv.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CvController } from './cv.controller';
 import { HandleCvService } from 'src/modules/services/handle-cv/handle-cv.service';
+import { ResourcesService } from 'src/modules/resources/resources.service';
+import { ResourcesModule } from 'src/modules/resources/resources.module';
 
 @Module({
   controllers: [CvController],
-  providers: [HandleCvService]
+  providers: [HandleCvService, ResourcesService],
+  imports: [ResourcesModule]
 })
 export class CvModule {}
