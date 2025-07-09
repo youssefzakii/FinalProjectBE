77487b79919ddbf4f3ec420c0bce4c0a59ea169c
ahmed ezzat <ahmedalahly800@gmail.com>
2025-07-09 15:50:01 +0300

    add get all companies

--- src/modules/company/company.controller.ts
+++ src/modules/company/company.controller.ts
@@ -6,12 +6,14 @@ import {
   Query,
   UploadedFile,
   UseInterceptors,
+  Req,
 
 } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { CompanyService } from "./company.service";
 import { SignUpCompanyDto, SignInCompanyDto } from "./dto/signup-company.dto";
 import { ApiConsumes, ApiBody, ApiBearerAuth } from "@nestjs/swagger";
+import { ForbiddenException } from "@nestjs/common";
 @Controller("/company")
 export class CompanyController {
   constructor(private readonly service: CompanyService) {}
@@ -31,11 +33,18 @@ export class CompanyController {
   async signIn(@Body() dto: SignInCompanyDto) {
     return this.service.signIn(dto);
   }
-
+  @ApiBearerAuth()
+  @Get("/allCompanies")
+  getAll(@Req () req: Request) {
+    if(req['user']?.role !== 'admin') throw new ForbiddenException('Unauthorized');
+    return this.service.getAll();
+  }
   @ApiBearerAuth()
   @Get("/search")
+
   search(@Query("field") field: string) {
     console.log('ok');
     return this.service.findByField(field);
   }
+
 }
